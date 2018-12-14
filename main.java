8e702502ac98f6de10cb526f1498e88cbe5c1d6a
zero25x <38110923+zero25x@users.noreply.github.com>
2018-12-14 11:53:09 +0100

    Update main.java

--- main.java
+++ main.java
@@ -5,11 +5,32 @@ import libs;
 class{
 
   public static void main(Strin args[]){
-  
-  
+    
+  int x,y,z;
+  String x;
+  class xx = new class();
+  scanner sc = new scanner();
+  x = sc.readLine();
+    
+    if (x == "wypisz"){
+    System.out.println("co mam wypisać?");
+      zmienna = pobierz tekst z klawiatury    
+      Wpisz_do_pliku("<p>" + zmienna + "</p>");
+    }
+    if(x=="wstaw"){
+    System.out.println("Co mam wstawić?");
+      if(x=="wykres"){
+      Wpisz_do_pliku("kod do wyświetlenia wykresu");
+      }
+      if(x=="karuzela"){
+      Wpisz_do_pliku("kod do wyświetlenia karuzeli")
+      }
+      x = pierwsza_linia_z_pliku_txt;
+      if(x=="tekst"){
+        
+      } 
+    }
     
     
-  
   }
-
 }
